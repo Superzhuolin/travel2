43e7b5800e8e5d6d31ea266b1966d57688f97dfc
Superzhuolin <1910185040@qq.com>
2022-09-30 04:19:57 +0800

    icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,24 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 <script>
 import HomeHeader from "./components/Header.vue";
 import HomeSwiper from "./components/Swiper.vue";
+import HomeIcons from "./components/Icons.vue";
+import HomeRecommend from "./components/Recommend.vue";
+import HomeWeekend from "./components/Weekend.vue";
 
 export default {
   name: "Home",
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend,
   },
   data() {
     return {
--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="iocons">
-    <swiper :options="swiperOption">
+  <div class="icons">
+    <swiper>
       <swiper-slide v-for="(page, index) of pages" :key="index">
-        <div class="icon" v-for="item of page" :key="item.id">
+        <div class="icon" v-for="item in page" :key="item.id">
           <div class="icon-img">
-            <img class="icon-img-content" :src="item.imgUrl" />
+            <img class="iocn-img-content" :src="item.imgUrl" />
           </div>
           <p class="icon-desc">{{ item.desc }}</p>
         </div>
@@ -15,21 +15,66 @@
 <script>
 export default {
   name: "HomeIcons",
-  props: {
-    list: Array,
-  },
   data() {
     return {
-      swiperOption: {
-        autoplay: false,
-      },
+      iconList: [
+        {
+          id: "0001",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/piao.png",
+          desc: "景点门票",
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "度假",
+        },
+        {
+          id: "0003",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/train.png",
+          desc: "火车票",
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/flight.png",
+          desc: "机票",
+        },
+        {
+          id: "0005",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/piao.png",
+          desc: "景点门票",
+        },
+        {
+          id: "0006",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "度假",
+        },
+        {
+          id: "0007",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/train.png",
+          desc: "火车票",
+        },
+        {
+          id: "0008",
+          imgUrl:
+            "https://s.qunarzz.com/homenode/images/touchheader/flight.png",
+          desc: "机票",
+        },
+        {
+          id: "0009",
+          imgUrl: "https://s.qunarzz.com/homenode/images/touchheader/hotel.png",
+          desc: "酒店",
+        },
+      ],
     };
   },
   computed: {
     pages() {
       const pages = [];
-      this.list.forEach((item, index) => {
-        const page = Math.floor(index / 8); //页码
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
         if (!pages[page]) {
           pages[page] = [];
         }
@@ -42,49 +87,50 @@ export default {
 </script>
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl';
+@import '~@/assets/styles/mixins.styl';
 
-/* 分页下方左右滑动未解决 */
 .icons/deep/.swiper-container {
   height: 0;
   padding-bottom: 50%;
 }
 
-.iocons {
-  .icon {
-    position: relative;
-    float: left;
-    width: 25%;
-    height: 0;
-    padding-bottom: 25%;
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
 
-    .icon-img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0.44rem;
-      box-sizing: border-box;
-      padding: 0.1rem;
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
 
-      .icon-img-content {
-        display: block;
-        margin: 0 auto;
-        height: 100%;
-      }
+    .iocn-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
     }
+  }
 
-    .icon-desc {
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 10px;
-      height: 0.44rem;
-      line-height: 0.44rem;
-      text-align: center;
-      color: $darkTextColor;
-      overflow: hidden;
-      white-space: nowrap;
-    }
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 10px;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    text-align: center;
+    color: $darkTextColor;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    ellipis()
   }
 }
 </style>
